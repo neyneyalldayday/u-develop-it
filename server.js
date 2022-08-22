36ba21c4f2f9fc512428acc67de9c9e6cac21308
neyneyalldayday <drfrankenstein123@gmail.com>
2022-08-22 16:18:07 -0500

    connected to the database

--- server.js
+++ server.js
@@ -2,14 +2,30 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const mysql = require('mysql2');
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'FrankY242424!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 app.get('/', (req, res) => {
     res.json({
       message: 'Hello World'
     });
+  });
+
+  db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
   });
 
   // Default response for any other request (Not Found)
